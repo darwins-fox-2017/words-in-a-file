272fa323198d6bb0e81ce2bce2d36998ff1949f2
Eri Irawan <eryirawan91@gmail.com>
2017-02-01 06:14:09 +0700

    replace not characater & push to array

--- words_in_a_file.js
+++ words_in_a_file.js
@@ -1,11 +1,21 @@
 var fs       = require('fs');
 var readFile = fs.readFileSync('source.txt','utf-8');
 
-function checkFile(dataFile) {
+function checkFile(dataFile, values) {
   if (!dataFile) {
     console.log('error');
   }else {
-    return readFile
+    let notChar = dataFile.replace(/[^A-z]+/gi, " ")
+        notChar = notChar.replace(/\[|\]/gi, " ")
+        notChar = notChar.split(" ")
+
+    let tempChar = []
+    for (let i = 0; i < notChar.length; i++) {
+      if(notChar[i] !== ""){
+        tempChar.push(notChar[i])
+      }
+    }
+    return tempChar
   }
 }
 
